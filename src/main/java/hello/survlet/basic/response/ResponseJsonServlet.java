6939b0a245b39334ec6b4ee5458ee4dd3bb36cee
younjaeyeal <jyyoun1021@gmail.com>
2022-06-16 03:31:26 +0900

    Http 응답 데이터 JSON

---
+++ src/main/java/hello/survlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,34 @@
+package hello.survlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.survlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet",urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper= new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type : application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("재열");
+        helloData.setAge(27);
+
+        //{"username" : "재열"."age" : 27}
+        String result = objectMapper.writeValueAsString(helloData);//객체를 JOSN 문자열로 변경!
+
+        response.getWriter().write(result);
+
+    }
+}
